73eb0f023d994d0a071fa8f2a20d39d0751e03d3
Gabriel Guimarães <gabriel.oliveira@ccc.ufcg.edu.br>
2016-11-23 15:43:28 -0300

    mudanças

--- SemTempo/app/src/main/java/com/example/semtempo/fragments/AddFragment.java
+++ SemTempo/app/src/main/java/com/example/semtempo/fragments/AddFragment.java
@@ -3,22 +3,36 @@ package com.example.semtempo.fragments;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.example.semtempo.R;
 
+import model.Atividade;
+import model.Prioridade;
+
 public class AddFragment extends Fragment {
 
     private final int SEND_ICON = R.drawable.ic_send_white_24dp;
-    private AutoCompleteTextView autoCompleteTextView;
+
     private String[] COUNTRIES = new String[] {
             "Belgium", "France", "Italy", "Germany", "Spain"
     };
+
+    private RadioButton high_priority;
+    private RadioButton medium_priority;
+    private RadioButton low_priority;
+    private EditText spent_time;
+    private EditText label_priority;
+    private AutoCompleteTextView autoCompleteTextView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,7 +46,20 @@ public class AddFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
-                Toast.makeText(getActivity(), "enviando", Toast.LENGTH_SHORT).show();
+                if(validateFields()) {
+                    Prioridade priority;
+                    if (high_priority.isSelected()) {
+                        priority = Prioridade.ALTA;
+                    } else if (medium_priority.isSelected()) {
+                        priority = Prioridade.MEDIA;
+                    } else {
+                        priority = Prioridade.BAIXA;
+                    }
+
+
+                    Atividade atividade = new Atividade(autoCompleteTextView.getText().toString(), null, null, priority);
+                    Toast.makeText(getActivity(), atividade.toString(), Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
@@ -42,7 +69,21 @@ public class AddFragment extends Fragment {
         autoCompleteTextView.setThreshold(1);
         autoCompleteTextView.setAdapter(adapter);
 
+        high_priority = (RadioButton) rootView.findViewById(R.id.high_radio);
+        medium_priority = (RadioButton) rootView.findViewById(R.id.medium_radio);
+        low_priority = (RadioButton) rootView.findViewById(R.id.low_radio);
+        label_priority = (EditText) rootView.findViewById(R.id.label_priority);
+        label_priority.setEnabled(false);
+
+        spent_time = (EditText) rootView.findViewById(R.id.spent_hours);
+
         return rootView;
+    }
+
+    private boolean validateFields(){
+        boolean fields_ok = true;
+
+        return fields_ok;
     }
 
 }
