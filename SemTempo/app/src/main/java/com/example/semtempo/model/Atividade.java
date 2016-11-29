de063ed2183ac997489fe81063070e8e97e26239
lucaspk <jobson.silva@ccc.ufcg.edu.br>
2016-11-29 02:35:20 -0300

    modificações

--- SemTempo/app/src/main/java/com/example/semtempo/model/Atividade.java
+++ SemTempo/app/src/main/java/com/example/semtempo/model/Atividade.java
@@ -15,15 +15,11 @@ import com.example.semtempo.model.Tag;
 public class Atividade {
 
     private String nomeDaAtv;
-    private Foto fotoDaAtividade;
     private Horario horariosRealizDaAtv;
-    private Tag categoria;
     private Prioridade prioridade;
     private String id;
 
-    public Atividade(){
-
-    }
+    public Atividade(){}
 
     public Atividade(String nomeDaAtv, Prioridade prioridade, Horario horario){
         this.nomeDaAtv = nomeDaAtv;
@@ -31,94 +27,30 @@ public class Atividade {
         this.horariosRealizDaAtv = horario;
     }
 
-    public Atividade(String nomeDaAtv, Foto fotoDaAtividade, Tag categoria, Prioridade prioridade, Horario horario){
-        this.nomeDaAtv = nomeDaAtv;
-        this.fotoDaAtividade = fotoDaAtividade;
-        this.categoria = categoria;
-        this.prioridade = prioridade;
-        this.horariosRealizDaAtv = horario;
-    }
+    public String getId(){ return id; }
 
-    public String getId(){
-        return id;
-    }
+    public void setId(String id){ this.id = id;}
 
-    public void setId(String id){
-        this.id = id;
-    }
+    public Horario getHorariosRealizDaAtv() { return horariosRealizDaAtv; }
 
-    public Foto getFotoDaAtividade() {
-        return fotoDaAtividade;
-    }
+    public void setHorariosRealizDaAtv(Horario horario){this.horariosRealizDaAtv = horario;}
 
-    public void alterarFotoDaAtividade(Foto fotoDaAtividade) {
-        this.fotoDaAtividade = fotoDaAtividade;
-    }
+    public Prioridade getPrioridade() { return prioridade;}
 
-    public Horario getHorariosRealizDaAtv() {
-        return horariosRealizDaAtv;
-    }
+    public void setPrioridade(Prioridade prioridade) { this.prioridade = prioridade;}
 
-    public void setListaHorarios(Horario horario){
-        this.horariosRealizDaAtv = horario;
-    }
+    public String getNomeDaAtv() { return nomeDaAtv; }
 
-    public Tag getCategoria() {
-        return categoria;
-    }
-
-    public void alterarCategoria(Tag categoria) {
-        this.categoria = categoria;
-    }
-
-    public Prioridade getPrioridade() {
-        return prioridade;
-    }
-
-    public void alterarPrioridade(Prioridade prioridade) {
-        this.prioridade = prioridade;
-    }
-
-    public String getNomeDaAtv() {
-        return nomeDaAtv;
-    }
-
-    public void alterarNomeDaAtv(String nomeDaAtv) {
-        this.nomeDaAtv = nomeDaAtv;
-    }
-
-    public void registrarNovoHorario(Horario tempoInvestido){
-        horariosRealizDaAtv = tempoInvestido;
-    }
+    public void alterarNomeDaAtv(String nomeDaAtv) { this.nomeDaAtv = nomeDaAtv; }
 
     @Override
-    public int hashCode() {
-        return nomeDaAtv.hashCode();
-    }
-
-    /**
-     * Retorna boolean pelo fato de que se um horário não consta na coleção, ele retornará falso.
-     * */
-//    public boolean removerHorario(Horario horarioEscolhido){
-//        return horariosRealizDaAtv.remove(horarioEscolhido);
-//    }
-
-//    public int getTotalDeHorasGasto(){
-//        int total = 0;
-//        for (Horario horario: getHorariosRealizDaAtv()){
-//            total += horario.getTotalHorasInvestidas();
-//        }
-//
-//        return total;
-//    }
+    public int hashCode() { return nomeDaAtv.hashCode(); }
 
     @Override
     public boolean equals(Object obj){
         if(!(obj instanceof Atividade)){
             return false;
         }
-
-
         Atividade atv = (Atividade) obj;
         return this.nomeDaAtv.equals(atv.getNomeDaAtv());
     }
